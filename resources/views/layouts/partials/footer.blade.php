3f69c9228ea739523994e38bd43bd6259ccd685c
sahriramadan000 <sahriramadan000@gmail.com>
2023-05-21 00:11:03 +0700

    Fix link footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,7 +1,7 @@
 
 <div class="fixed bottom-0 left-0 z-50 w-full h-16 sm:h-14 bg-white shadow-footer dark:bg-[#111827]">
     <div class="grid h-full max-w-lg grid-cols-3 mx-auto font-medium">
-        <button type="button" class="inline-flex flex-col items-center justify-center px-5 py-2 group">
+        <button type="button" class="inline-flex flex-col items-center justify-center px-5 py-2 group" onclick="location.href='{{ route('homepage') }}';">
             <svg class="w-5 h-5 sm:w-5 sm:h-5 mb-[5px] text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                 <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/>
             </svg>
@@ -26,7 +26,7 @@
             </svg>
             <span class="text-xs text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Settings</span>
         </button> --}}
-        <button type="button" class="inline-flex flex-col items-center justify-center px-5 py-2 group">
+        <button type="button" class="inline-flex flex-col items-center justify-center px-5 py-2 group" onclick="location.href='{{ route('users.edit', Auth::user()->id) }}';">
             <svg class="w-6 h-6 mb-[5px] text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-800" fill="currentColor" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                 <path d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/>
             </svg>
