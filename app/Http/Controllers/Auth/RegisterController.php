d15435dd12f7da644d44b250739891fcadfbfd2c
syahrul0505 <syahrulalif1234@gmail.com>
2023-06-09 16:53:47 +0700

    regis controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
 
         $user = new User();
         $user->username     = $validate['username'];
-        $user->name         = $validate['username'];
+        // $user->name         = $validate['username'];
         $user->telephone    = $validate['telephone'];
         $user->password     = Hash::make($validate['password']);
         $user->save();
