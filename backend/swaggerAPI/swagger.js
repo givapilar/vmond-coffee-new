7656f3a3b7da3b83e8be47e7f6e52c572a631b2b
givapilar <givapilarsemesta@gmail.com>
2023-08-23 20:51:28 +0000

    swagger api

--- backend/swaggerAPI/swagger.js
+++ backend/swaggerAPI/swagger.js
@@ -1,15 +1,16 @@
 const swaggerJSDoc = require('swagger-jsdoc');
+const path = require('path'); // Import module path
 
 const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Express API with Swagger',
+      title: 'Dokumentation API with Swagger',
       version: '1.0.0',
       description: 'A sample API with Swagger documentation',
     },
   },
-  apis: ['../routes/*.js'], // Letakkan sesuai dengan path file-route Anda
+  apis: [path.resolve(__dirname, '../routes/*.js')], // Menggunakan path.resolve untuk menyesuaikan path
 };
 
 const swaggerSpec = swaggerJSDoc(options);
