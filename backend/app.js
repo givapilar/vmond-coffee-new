7a960f04bb93d04c1aa09d2cbb09e17151c0da7b
givapilar <givapilarsemesta@gmail.com>
2023-08-23 20:42:36 +0000

    swagger api

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/integrasiApiRoutes');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swaggerAPI/swagger'); // Sesuaikan path jika berbeda
 // Middleware untuk mengizinkan parsing JSON dari permintaan
 app.use(express.json());
-
+app.use('/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 // Gunakan rute untuk pengguna
 app.use('/v1/api', userRoutes);
-
 // Jalankan server pada port tertentu
 const port = 2222;
 app.listen(port, () => {
