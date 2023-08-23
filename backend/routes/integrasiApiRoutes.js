e3be336779339b62ecbfce1532cf4b54c7d29ef4
givapilar <givapilarsemesta@gmail.com>
2023-08-23 21:27:02 +0000

    swagger api

--- backend/routes/integrasiApiRoutes.js
+++ backend/routes/integrasiApiRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const integrasiApiController = require('../controllers/integrasiApiController');
 
-// Definisikan rute untuk mendapatkan semua pengguna
 router.get('/', integrasiApiController.getURL);
 
 /**
@@ -28,7 +27,6 @@ router.get('/', integrasiApiController.getURL);
  *               message: Failed!.
  */
 
-// Definisikan rute untuk mendapatkan pengguna berdasarkan ID
 router.post('/callback', integrasiApiController.callbackFromBJB);
 
 module.exports = router;
