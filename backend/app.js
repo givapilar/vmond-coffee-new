707301d4a800f3a496760a9e8001439a0097363e
givapilar <givapilarsemesta@gmail.com>
2023-08-31 01:39:21 +0000

    api

--- backend/app.js
+++ backend/app.js
@@ -139,63 +139,63 @@ const passwordDev = "51cbc137951976fa96deaa8899ce7dba641e0f309b8d50e02698436f893
 
 // Aktivasi
 
-const aktivasi = async () => {
-  try {
-    const headers = {
-      "Content-Type": "application/json",
-      "X-AUTH-TOKEN": "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiIxYWVhNjczNi1jNjU5LTQwNmEtOTRlNi00NTI2YzdlNjVhYTgiLCJzdWIiOiJWTU85MzcxOTI5ODgiLCJleHAiOjE2OTM0MjMwMTAsImlhdCI6MTY5MzM5MzAxMCwiaWRlbnRpZmllciI6IkVKSkJheTFEcVVYWHFCSyIsInVzZXJuYW1lIjoiVk1POTM3MTkyOTg4In0.7YzRn3AM7LV9YnLdHBi87nhSYvzkyL4V7M2ZAvZZ8h7hl-z4WAaCiAK1NpmFAkjCI5zfW1bmE_WAbXbJLkapUg" // Replace with actual X-AUTH-TOKEN value
-    };
+// const aktivasi = async () => {
+//   try {
+//     const headers = {
+//       "Content-Type": "application/json",
+//       "X-AUTH-TOKEN": "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiIxYWVhNjczNi1jNjU5LTQwNmEtOTRlNi00NTI2YzdlNjVhYTgiLCJzdWIiOiJWTU85MzcxOTI5ODgiLCJleHAiOjE2OTM0MjMwMTAsImlhdCI6MTY5MzM5MzAxMCwiaWRlbnRpZmllciI6IkVKSkJheTFEcVVYWHFCSyIsInVzZXJuYW1lIjoiVk1POTM3MTkyOTg4In0.7YzRn3AM7LV9YnLdHBi87nhSYvzkyL4V7M2ZAvZZ8h7hl-z4WAaCiAK1NpmFAkjCI5zfW1bmE_WAbXbJLkapUg" // Replace with actual X-AUTH-TOKEN value
+//     };
     
-    const metaData = {
-      "datetime": "2023-08-30T09:40:21.450Z",
-      "deviceId": "bjbdigi",
-      "devicePlatform": "Linux",
-      "deviceOSVersion": "bjbdigi-version",
-      "deviceType": "",
-      "latitude": "",
-      "longitude": "",
-      "appId": 4,
-      "appVersion": "1.0",
-    };
+//     const metaData = {
+//       "datetime": "2023-08-30T09:40:21.450Z",
+//       "deviceId": "bjbdigi",
+//       "devicePlatform": "Linux",
+//       "deviceOSVersion": "bjbdigi-version",
+//       "deviceType": "",
+//       "latitude": "",
+//       "longitude": "",
+//       "appId": 4,
+//       "appVersion": "1.0",
+//     };
     
-    const bodyData = {
-      "msisdn": "081717181988",
-      "pin": "123456",
-      "reference": "04832a86-313e-4a1c-9b92-ad8399ca3e3a",
-      "product": "MERCHANT",
-    };
+//     const bodyData = {
+//       "msisdn": "081717181988",
+//       "pin": "123456",
+//       "reference": "04832a86-313e-4a1c-9b92-ad8399ca3e3a",
+//       "product": "MERCHANT",
+//     };
     
-    const result = await axios.post(urlGlobal + "/mobile-webconsole/apps/4/pbNonFinancialAdapter//authorizationRegistration", 
-      {"metadata": metaData, "body": bodyData}, 
-      {"headers": headers}
-    );
+//     const result = await axios.post(urlGlobal + "/mobile-webconsole/apps/4/pbNonFinancialAdapter//authorizationRegistration", 
+//       {"metadata": metaData, "body": bodyData}, 
+//       {"headers": headers}
+//     );
     
-    console.log("AllResult :: ", result);
-    console.log("Result Data :: ", result.data);
+//     console.log("AllResult :: ", result);
+//     console.log("Result Data :: ", result.data);
     
 
-    if (result && result.data && result.data.body && result.data.body.AuthorizationRegisterResponse) {
-      const response = result.data.body.AuthorizationRegisterResponse;
-      const attr = response._attr;
-      const customer = response.customer;
-      const xAuthToken = result.headers['x-auth-token'];
+//     if (result && result.data && result.data.body && result.data.body.AuthorizationRegisterResponse) {
+//       const response = result.data.body.AuthorizationRegisterResponse;
+//       const attr = response._attr;
+//       const customer = response.customer;
+//       const xAuthToken = result.headers['x-auth-token'];
 
-      console.log("X-AUTH-TOKEN:", xAuthToken)
-      console.log("Attr:", attr);
-      console.log("Customer:", customer);
-    } else {
-      console.log("Response structure is not as expected.");
-    }
+//       console.log("X-AUTH-TOKEN:", xAuthToken)
+//       console.log("Attr:", attr);
+//       console.log("Customer:", customer);
+//     } else {
+//       console.log("Response structure is not as expected.");
+//     }
     
     
-  } catch (err) {
-    // Handle Error Here
-    console.error(err);
-  }
-};
+//   } catch (err) {
+//     // Handle Error Here
+//     console.error(err);
+//   }
+// };
 
 
-aktivasi();
+// aktivasi();
 
 // Request Token for Transaction Authentication as Merchant
 // const requestTokenAuthMerchant = async () => {
@@ -257,63 +257,63 @@ aktivasi();
 
 // ====================================================================
 // Create Qris Dinamis BJB
-// const createQrisFintech = async () => {
-//   try {
-//     const headers = {
-//       "Content-Type": "application/json",
-//       "X-AUTH-TOKEN": "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiI1ZGRjOTI0NC01ZDdhLTRhNjItOTdmNi1mNTg0ZmY1ZDk1ZjMiLCJzdWIiOiJ0aGlyZHBhcnR5MDEiLCJleHAiOjE2OTM0MDMwNjcsImlhdCI6MTY5MzM3MzA2NywiaWRlbnRpZmllciI6ImtkZXpLSUpNR3JTdWlEbSIsInVzZXJuYW1lIjoidGhpcmRwYXJ0eTAxIn0.yzz5VkMJdkHfc66V4Svhhsngk7BtZrildMiPe1QQDlIl5CvajicDwMngqt5OdnOMTPW0ikRTiPIAoqKQEZRmcw" // Replace with actual X-AUTH-TOKEN value
-//     };
+const createQrisFintech = async () => {
+  try {
+    const headers = {
+      "Content-Type": "application/json",
+      "X-AUTH-TOKEN": "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiIxYWVhNjczNi1jNjU5LTQwNmEtOTRlNi00NTI2YzdlNjVhYTgiLCJzdWIiOiJWTU85MzcxOTI5ODgiLCJleHAiOjE2OTM0MjMwMTAsImlhdCI6MTY5MzM5MzAxMCwiaWRlbnRpZmllciI6IkVKSkJheTFEcVVYWHFCSyIsInVzZXJuYW1lIjoiVk1POTM3MTkyOTg4In0.7YzRn3AM7LV9YnLdHBi87nhSYvzkyL4V7M2ZAvZZ8h7hl-z4WAaCiAK1NpmFAkjCI5zfW1bmE_WAbXbJLkapUg" // Replace with actual X-AUTH-TOKEN value
+    };
     
-//     const metaData = {
-//       "datetime": "2023-08-30T09:40:21.450Z",
-//       "deviceId": "bjbdigi",
-//       "devicePlatform": "Linux",
-//       "deviceOSVersion": "bjbdigi-version",
-//       "deviceType": "",
-//       "latitude": "",
-//       "longitude": "",
-//       "appId": 4,
-//       "appVersion": "1.0",
-//       "appIdName": "Third Party 01" // Replace with actual appIdName value
-//     };
+    const metaData = {
+      "datetime": "2023-08-30T09:40:21.450Z",
+      "deviceId": "bjbdigi",
+      "devicePlatform": "Linux",
+      "deviceOSVersion": "bjbdigi-version",
+      "deviceType": "",
+      "latitude": "",
+      "longitude": "",
+      "appId": 4,
+      "appVersion": "1.0",
+      "appIdName": "vmondcoffee" // Replace with actual appIdName value
+    };
     
-//     const bodyData = {
-//       "merchantAccountNumber": msisdnDev, // Replace with actual merchant account number
-//       "amount": "1000", // Replace with actual amount
-//       "expInSecond": 3600 // Replace with actual expiry in seconds or remove if not needed
-//     };
+    const bodyData = {
+      "merchantAccountNumber": msisdnDev, // Replace with actual merchant account number
+      "amount": "1000", // Replace with actual amount
+      "expInSecond": 3600 // Replace with actual expiry in seconds or remove if not needed
+    };
     
-//     const result = await axios.post(urlGlobal + "/mobile-webconsole/apps/4/pbTransactionAdapter/createInvoiceQRISDinamisExt", 
-//       {"metadata": metaData, "body": bodyData}, 
-//       {"headers": headers}
-//     );
+    const result = await axios.post(urlGlobal + "/mobile-webconsole/apps/4/pbTransactionAdapter/createInvoiceQRISDinamisExt", 
+      {"metadata": metaData, "body": bodyData}, 
+      {"headers": headers}
+    );
     
-//     console.log("AllResult :: ", result);
-//     console.log("Result Data :: ", result.data);
+    console.log("AllResult :: ", result);
+    console.log("Result Data :: ", result.data);
     
 
-//     // Check Console detail qris
-//     if (result && result.data && result.data.body && result.data.body.CreateInvoiceQRISDinamisExtResponse) {
-//       const response = result.data.body.CreateInvoiceQRISDinamisExtResponse;
-//       const attr = response._attr;
-//       const responseCode = response.responseCode;
-//       const responseMessage = response.responseMessage;
+    // Check Console detail qris
+    if (result && result.data && result.data.body && result.data.body.CreateInvoiceQRISDinamisExtResponse) {
+      const response = result.data.body.CreateInvoiceQRISDinamisExtResponse;
+      const attr = response._attr;
+      const responseCode = response.responseCode;
+      const responseMessage = response.responseMessage;
     
-//       console.log("Attr:", attr);
-//       console.log("Response Code:", responseCode);
-//       console.log("Response Message:", responseMessage);
-//     } else {
-//       console.log("Response structure is not as expected.");
-//     }
+      console.log("Attr:", attr);
+      console.log("Response Code:", responseCode);
+      console.log("Response Message:", responseMessage);
+    } else {
+      console.log("Response structure is not as expected.");
+    }
     
-//   } catch (err) {
-//     // Handle Error Here
-//     console.error(err);
-//   }
-// };
+  } catch (err) {
+    // Handle Error Here
+    console.error(err);
+  }
+};
 
 
-// createQrisFintech();
+createQrisFintech();
 
 // Jalankan server pada port tertentu
 const port = 2222;
