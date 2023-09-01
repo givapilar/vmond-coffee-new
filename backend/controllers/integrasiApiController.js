5ec928f49f2629b07854cb889807a76d5de5823f
givapilar <givapilarsemesta@gmail.com>
2023-09-01 06:24:33 +0000

    api

--- backend/controllers/integrasiApiController.js
+++ backend/controllers/integrasiApiController.js
@@ -1,8 +1,35 @@
 const jwt = require('jwt-simple');
 const secretKey = 'yUyOSXBD8ZB96JKv5e5K4aETVLJcGkubL8d6UlrqERJSVtvDJr';
+const { exec } = require('child_process');
 
 const getURL = (req, res) => {
     const resCallback = "Success Connect!";
+    res.json(resCallback);
+  };
+
+  const requestToken = (req, res) => {
+    const curlCommand = `
+      curl -X POST "http://172.31.32.85:2222/v1/api/" \
+      -H "Content-Type: application/json" \
+      -d '{
+        "metadata": {
+          "datetime": "2023-09-01T09:40:21.450Z",
+          "deviceId": "bjbdigi",
+          "devicePlatform": "Linux",
+          "deviceOSVersion": "bjbdigi-version",
+          "deviceType": "",
+          "latitude": "",
+          "longitude": "",
+          "appId": 4,
+          "appVersion": "1.0"
+        },
+        "body": {
+          "msisdn": "your_msisdn_value",
+          "password": "1c1bbf7b79bc9b97cafb7488946a6001f05980f62dbeb5bc093dd680b8241197"
+        }
+      }'
+    `;
+  
     res.json(resCallback);
   };
   
@@ -47,5 +74,6 @@ const getURL = (req, res) => {
   
   module.exports = {
     getURL,
-    callbackFromBJB
+    callbackFromBJB,
+    requestToken,
   };
