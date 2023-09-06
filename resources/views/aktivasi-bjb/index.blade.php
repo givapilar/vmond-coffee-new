3a7d0740ce3c00c66c1e4daadf224039481948f2
givapilar <givapilarsemesta@gmail.com>
2023-09-06 18:44:35 +0000

    update

--- resources/views/aktivasi-bjb/index.blade.php
+++ resources/views/aktivasi-bjb/index.blade.php
@@ -9,8 +9,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container">
+        <button onclick="getToken()" class="btn btn-sm btn-primary">Get Token</button>
+    </div>
     {{-- <button onclick="test()">Test</button> --}}
-    <button onclick="getToken()" class="btn btn-sm btn-primary">Get Token</button>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
