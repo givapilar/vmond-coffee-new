0dd9871cbe3ba9b5168a7d6c95120fc90ce8307e
givapilar <givapilarsemesta@gmail.com>
2023-09-06 15:17:29 +0000

    udpate

--- backend/controllers/integrasiApiController.js
+++ backend/controllers/integrasiApiController.js
@@ -49,43 +49,44 @@ const callbackFromBJB = (req, res) => {
     }
 };
 
-const getTokenFintech = (req, res) => {
+const getTokenFintech = async (req, res) => {
     try {
-        // const headers = {
-        //     'Content-Type': 'application/json',
-        // };
+        const dtmsisdn = req.body.msisdnDev;
+        const dtpassword = req.body.passwordDev;
+        
+        const headers = {
+            'Content-Type': 'application/json',
+        };
 
-        // const metaData = {
-        //     "datetime": "2023-09-05T09:40:21.450Z",
-        //     "deviceId": "9f9cb0504caa5059", 
-        //     "devicePlatform": "Linux",
-        //     "deviceOSVersion": "9",
-        //     "deviceType": "",
-        //     "latitude": "",
-        //     "longitude": "",
-        //     "appId": 4,
-        //     "appVersion": "1.0",
-        // };
+        const metaData = {
+            "datetime": "2023-09-05T09:40:21.450Z",
+            "deviceId": "9f9cb0504caa5059", 
+            "devicePlatform": "Linux",
+            "deviceOSVersion": "9",
+            "deviceType": "",
+            "latitude": "",
+            "longitude": "",
+            "appId": 4,
+            "appVersion": "1.0",
+        };
 
-        // const bodyData = {
-        //     msisdn: req.msisdnDev,
-        //     password: req.passwordDev
-        // };
+        const bodyData = {
+            msisdn: dtmsisdn,
+            password: dtpassword
+        };
 
-        // const result = await axios.post(
-        //     urlGlobal + '/mobile-webconsole/apps/pocket/requestTokenFintech/',
-        //     { metadata: metaData, body: bodyData },
-        //     { headers: headers }
-        // );
+        const result = await axios.post(
+            urlGlobal + '/mobile-webconsole/apps/pocket/requestTokenFintech/',
+            { metadata: metaData, body: bodyData },
+            { headers: headers }
+        );
 
-        // const xAuthToken = result.headers['x-auth-token'];
-        const msisdn = req.body.msisdnDev;
-        const password = req.body.passwordDev;
+        const xAuthToken = result.headers['x-auth-token'];
+        
         const responseData = {
             code: 200,
             method: req.method,
-            data1: msisdn,
-            data2: password,
+            token: xAuthToken,
             message: 'Successfully!'
         };
         res.status(200).json(responseData);
