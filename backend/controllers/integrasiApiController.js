ecd43d7fd04a5682afd116c3f585e9e07d5868c2
givapilar <givapilarsemesta@gmail.com>
2023-09-06 14:57:26 +0000

    udpate

--- backend/controllers/integrasiApiController.js
+++ backend/controllers/integrasiApiController.js
@@ -49,36 +49,9 @@ const callbackFromBJB = (req, res) => {
     }
 };
 
-const getTokenFintech = async (req, res) => {
+const getTokenFintech = (req, res) => {
     try {
-        // const headers = {
-        //     'Content-Type': 'application/json',
-        // };
-
-        // const metaData = {
-        //     "datetime": "2023-09-05T09:40:21.450Z",
-        //     "deviceId": "9f9cb0504caa5059", 
-        //     "devicePlatform": "Linux",
-        //     "deviceOSVersion": "9",
-        //     "deviceType": "",
-        //     "latitude": "",
-        //     "longitude": "",
-        //     "appId": 4,
-        //     "appVersion": "1.0",
-        // };
-
-        // const bodyData = {
-        //     msisdn: req.msisdnDev,
-        //     password: req.passwordDev
-        // };
-
-        // const result = await axios.post(
-        //     urlGlobal + '/mobile-webconsole/apps/pocket/requestTokenFintech/',
-        //     { metadata: metaData, body: bodyData },
-        //     { headers: headers }
-        // );
-
-        // const xAuthToken = result.headers['x-auth-token'];
+       
 
         const responseData = {
             code: 200,
