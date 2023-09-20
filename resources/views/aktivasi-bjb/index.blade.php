b2f55334982525e57361d044b8f711c67ca2d299
givapilar <givapilarsemesta@gmail.com>
2023-09-20 16:53:03 +0000

    qr

--- resources/views/aktivasi-bjb/index.blade.php
+++ resources/views/aktivasi-bjb/index.blade.php
@@ -33,12 +33,12 @@
                         <button data-modal-toggle="deleteModal" type="button" class="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
                             No, cancel
                         </button>
-                        <form action="{{ route('checkout-waiters') }}" method="POST">
-                            @csrf
+                        {{-- <form action="{{ route('checkout-waiters') }}" method="POST"> --}}
+                            {{-- @csrf --}}
                             <button type="submit" class="btn btn-danger py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
                                 Yes, I'm sure
                             </button>
-                        </form>                            
+                        {{-- </form>                             --}}
                     </div>
                 </div>
             </div>
