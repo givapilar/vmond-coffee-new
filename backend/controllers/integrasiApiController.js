98d708254b9890bcdd7441039a93ba5fb51c6976
givapilar <givapilarsemesta@gmail.com>
2023-09-21 16:29:11 +0000

    api

--- backend/controllers/integrasiApiController.js
+++ backend/controllers/integrasiApiController.js
@@ -48,6 +48,7 @@ const callbackFromBJB = (req, res) => {
         
         // Lakukan sesuatu dengan data dari tubuh permintaan
         console.log('Request Body:', requestBody);
+        console.log('Customer Name:', requestBody.customerName);
         
         // console.log('Request : ',req);
 
