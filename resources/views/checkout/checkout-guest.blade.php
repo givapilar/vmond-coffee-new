f491b835094e2d11cc0c5eaa5653a5256be3b067
givapilar <givapilarsemesta@gmail.com>
2023-09-21 19:56:10 +0000

    api

--- resources/views/checkout/checkout-guest.blade.php
+++ resources/views/checkout/checkout-guest.blade.php
@@ -237,7 +237,7 @@
             <button id="pay-button" class="w-full h-full p-3 bg-blue-500 dark:text-white rounded-b-[30px] hover:bg-blue-700 focus:ring-2 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-900">Order Now</button>
         </div> --}}
         <div class="mt-2">
-            <button id="btnQR" onclick="createQris('{{ $orders->total_price }}', '{{ $oder->id }}')" class="w-full h-full p-3 bg-blue-500 dark:text-white rounded-b-[30px] hover:bg-blue-700 focus:ring-2 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-900">Order Now</button>
+            <button id="btnQR" onclick="createQris('{{ $orders->total_price }}', '{{ $orders->id }}')" class="w-full h-full p-3 bg-blue-500 dark:text-white rounded-b-[30px] hover:bg-blue-700 focus:ring-2 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-900">Order Now</button>
         </div>
         @endif 
 
