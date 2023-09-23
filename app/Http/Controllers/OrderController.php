91b05a3d622d17663406571eabd4ee8d325f4a51
givapilar <givapilarsemesta@gmail.com>
2023-09-23 06:54:27 +0000

    api

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2100,8 +2100,8 @@ class OrderController extends Controller
         try {
             // $order = Order::where('invoice_id',$request->invoiceID);
 
-            // $order = Order::where('invoice_id', $request->invoiceID)->first();
-            // $order->update(['description' => 'SUKSES']);
+            $order = Order::where('invoice_id', $request->body->invoiceID)->first();
+            $order->update(['description' => 'SUKSES']);
 
             // if ($order->meja_restaurant_id != null || $order->category == 'Takeaway') {
             //     $userID = $order->user_id;
