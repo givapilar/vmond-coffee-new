e1f62f57525f92c928abff786b694b260265e2b8
givapilar <givapilarsemesta@gmail.com>
2023-09-24 08:32:18 +0000

    api

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -372,7 +372,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/canvg/1.5/canvg.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/qrious/4.0.2/qrious.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.2.1/axios.min.js"></script>
-<script src="https://cdn.socket.io/socket.io-4.1.7.js"></script>
+<script src="https://cdn.socket.io/4.6.0/socket.io.min.js" integrity="sha384-c79GN5VsunZvi+Q/WObgk2in0CbZsHnjEqvFxC5DxHn9lTfNce2WW6h2pH6u/kF+" crossorigin="anonymous"></script>
 
 <script>
     var socket = io('http://vmondcoffee.controlindo.com:2222');
