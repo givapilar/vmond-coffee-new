9e1c1e056907b496d2f6b97f60fdea293962571d
givapilar <givapilarsemesta@gmail.com>
2023-09-24 09:21:45 +0000

    add socket io

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -375,7 +375,7 @@
 <script src="https://cdn.socket.io/4.6.0/socket.io.min.js" integrity="sha384-c79GN5VsunZvi+Q/WObgk2in0CbZsHnjEqvFxC5DxHn9lTfNce2WW6h2pH6u/kF+" crossorigin="anonymous"></script>
 
 <script>
-    var socket = io('https://socket-vmondcoffee.controlindo.com:443', { transports: ["websocket"] });
+    var socket = io('https://socket-vmondcoffee.controlindo.com:443', { forceNew: true });
 
     socket.on('paymentSuccess', function (msg) {
         console.log("HEREEEEE", msg);
