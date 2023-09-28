b6609aacb4b92100651704a1c2e09dc0c9e0a9c9
givapilar <givapilarsemesta@gmail.com>
2023-09-28 12:26:53 +0000

    update QR

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -380,7 +380,7 @@
                                 </p>
                             </div>
                             <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                <input required type="radio" id="payment_gateaway" value="BJB-Testing" name="tipe_pemesanan" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <input required type="radio" id="payment_gateaway" value="QR-BJB" name="tipe_pemesanan" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                             </div>
                             </div>
                         </li>
