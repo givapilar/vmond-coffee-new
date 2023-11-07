42c150f72e17b444685d299b69bd4ae8b460e2d4
syahrul0505 <syahrulalif1234@gmail.com>
2023-11-07 09:11:01 +0700

    bjb

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -364,9 +364,9 @@
             </div>
        
         @elseif($order_last->tipe_pemesanan == 'QR-BJB')
-            {{-- <div class="mt-2">
+            <div class="mt-2">
                 <button id="btnQR" onclick="createQris('{{ $order_last->total_price }}', '{{ $order_last->id }}')" class="w-full h-full p-3 bg-blue-500 dark:text-white rounded-b-[30px] hover:bg-blue-700 focus:ring-2 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-900">Order Now</button>
-            </div> --}}
+            </div>
         @else
         <div class="mt-2">
             <button id="pay-button" class="w-full h-full p-3 bg-blue-500 dark:text-white rounded-b-[30px] hover:bg-blue-700 focus:ring-2 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-900">Order Now</button>
