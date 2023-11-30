a4dae755b175f8effe4158f3bba2860007b7f8e1
syahrul0505 <syahrulalif1234@gmail.com>
2023-11-30 22:51:31 +0700

    edit payment BJB

--- backend/controllers/integrasiApiController.js
+++ backend/controllers/integrasiApiController.js
@@ -472,6 +472,7 @@ const checkStatusPay = async (req, res) => {
         .then((response) => {
             // Permintaan berhasil
             const responseData = response.data;
+            
             console.log('Berhasil:', responseData);
         })
         .catch((error) => {
