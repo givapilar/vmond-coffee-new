66e0fbf6d3d9b974fec48adc89ac5d802e61d72b
Sahri Ramadan <sahriramadan000@gmail.com>
2024-04-30 03:08:26 +0700

    history transaction

--- resources/views/modal/history.blade.php
+++ resources/views/modal/history.blade.php
@@ -69,3 +69,14 @@
         </div>
     </div>
 </div>
+
+@push('script-bot')
+    <script>
+        function closeModal(itemCode) {
+            var modal = document.getElementById('feedback-modal' + itemCode);
+            modal.classList.add('hidden');
+            modal.setAttribute('aria-hidden', 'true');
+            modal.setAttribute('tabindex', '-1');
+        }
+    </script>
+@endpush
