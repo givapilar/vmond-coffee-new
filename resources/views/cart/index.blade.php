0b030c1cffc67d66b4bed61a60d1222d0c10f7b0
syahrul0505 <syahrulalif1234@gmail.com>
2024-04-30 03:30:56 +0700

    update email

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -270,7 +270,7 @@
                                     </p>
                                 </div>
                             </div>
-                            <input required placeholder="Ex: xxx@gmail.com" name="email" value="{{ old('email') }}" id="" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2 mt-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('password') is-invalid @enderror">
+                            <input placeholder="Ex: xxx@gmail.com" name="email" value="{{ old('email') }}" id="" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2 mt-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('password') is-invalid @enderror">
                         </li>
                         </ul>
                     </div>
