173fd85e5f7d5a2936f47f95681ccf4c61eaf1a6
Sahri Ramadan <sahriramadan000@gmail.com>
2024-07-22 07:14:14 +0700

    feat: add openbill in billiard

--- resources/views/daftarmenu/billiard.blade.php
+++ resources/views/daftarmenu/billiard.blade.php
@@ -131,23 +131,27 @@
             @endif
         @endforeach
 
-        <div class="text-base sm:text-sm px-1 py-3">
-            <div class="aspect-h-1 h-24 aspect-w-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
-                <img src="https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="." class="object-cover object-center h-full w-full">
-            </div>
-            <div class="px-1">
-                <p aria-hidden="true" class="text-xs mt-1 font-semibold dark:text-gray-300">Open Bill Package</p>
-                <span class="block text-[10px] dark:text-red-500">Rp.- </span>
+        @if(Auth::user())
+        @if(Auth::user()->telephone == '081818181847')
+            <div class="text-base sm:text-sm px-1 py-3">
+                <div class="aspect-h-1 h-24 aspect-w-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                    <img src="https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="." class="object-cover object-center h-full w-full">
+                </div>
+                <div class="px-1">
+                    <p aria-hidden="true" class="text-xs mt-1 font-semibold dark:text-gray-300">Open Bill Package</p>
+                    <span class="block text-[10px] dark:text-red-500">Rp.- </span>
 
-                <div class="flex gap-1 opacity-75 mt-auto">
-                    <form action="{{ route('detail-billiard-openbill') }}" method="get" class=" w-8/12">
-                        <div class="flex gap-1 opacity-75">
-                            <button class="w-full bg-sky-500 text-xs rounded-lg mt-2 p-1 hover:bg-sky-800 focus:outline-none focus:ring-2 focus:ring-sky-300"><ion-icon name="bag-add" class="mt-[0.2rem] dark:text-white"></ion-icon></button>
-                        </div>
-                    </form>
+                    <div class="flex gap-1 opacity-75 mt-auto">
+                        <form action="{{ route('detail-billiard-openbill') }}" method="get" class=" w-8/12">
+                            <div class="flex gap-1 opacity-75">
+                                <button class="w-full bg-sky-500 text-xs rounded-lg mt-2 p-1 hover:bg-sky-800 focus:outline-none focus:ring-2 focus:ring-sky-300"><ion-icon name="bag-add" class="mt-[0.2rem] dark:text-white"></ion-icon></button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+            @endif
+        @endif
     </div>
 </section>
 
