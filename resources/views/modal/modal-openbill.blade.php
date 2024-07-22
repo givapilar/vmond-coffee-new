4024a2a3a4ab5d81791594e16981d669b8b4f3c4
Sahri Ramadan <sahriramadan000@gmail.com>
2024-07-22 11:02:29 +0700

    Fix: update edc by class

--- resources/views/modal/modal-openbill.blade.php
+++ resources/views/modal/modal-openbill.blade.php
@@ -41,11 +41,11 @@
                             <p class="text-sm font-medium text-gray-900 truncate dark:text-white">EDC</p>
                         </div>
                         <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                            <input required type="radio" id="edisi-{{ $item->id }}" value="Edisi" name="tipe_pemesanan" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                            <input required type="radio" class="edisi-radio edisi-radio w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                         </div>
                     </div>
                 </li>
-                <li class="py-3 sm:py-2" style="display:none;" id="edisi-pembayaran-{{ $item->id }}">
+                <li class="py-3 sm:py-2 edisi-pembayaran-wrapper edisi-pembayaran-wrapper" style="display:none;">
                     <div id="select-input-wrapper">
                         <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pilih EDC</label>
                         <select id="countries" name="metode_edisi" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -67,28 +67,4 @@
         </form>
     </div>
 </div>
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/canvg/1.5/canvg.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/qrious/4.0.2/qrious.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.2.1/axios.min.js"></script>
-<script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
-<script src="{{ asset('assetku/dataku/js/socket.io.js') }}"></script> --}}
-<script>
-    // const paymentGatewayRadio = document.getElementById('payment_gateaway');
-    const itemId = '{{ $item->id }}';
-    const edcRadio = document.getElementById(`edisi-${itemId}`);
-    const edisiPembayaranWrapper = document.getElementById(`edisi-pembayaran-${itemId}`);
 
-    // paymentGatewayRadio.addEventListener('change', function() {
-    //     if (this.checked) {
-    //         edisiPembayaranWrapper.style.display = 'none';
-
-    //     }
-    // });
-
-    edcRadio.addEventListener('change', function() {
-        if (this.checked) {
-            edisiPembayaranWrapper.style.display = 'block';
-        }
-    });
-
-</script>
